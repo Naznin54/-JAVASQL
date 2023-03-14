ec628cefa8adbfc44d39ffc8ad9c6440197a3a7c
Naznin54 <89862769+Naznin54@users.noreply.github.com>
2023-03-14 11:48:28 +0530

    Delete mysqlcon.sql

--- mysqlcon.sql
+++
@@ -1,8 +0,0 @@
-create database sonoo;  
-use sonoo;  
-create table emp(id int(10),name varchar(40),age int(3));  
-insert into emp
-values(1,"naz",22);
-select *from employee;
-INSERT INTO employ (id, name, age, salary)
-values(1,"nnmm",22,50000);
